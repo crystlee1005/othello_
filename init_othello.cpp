f65d60ffa52e187e3c6a3e703050eb1f6ee2f681
crystlee1005 <crystlee1005@sookmyung.ac.kr>
2020-11-14 22:33:21 +0900

    11.14

--- init_othello.cpp
+++ init_othello.cpp
@@ -3,7 +3,7 @@
 #define N 6
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-
+char gameboard[N][N];
 
 int main(void){
   // 필요한 변수들 정의 
@@ -14,17 +14,25 @@ int main(void){
   
   for(i=0; i<N; i++)
      for(j=0; j<N; j++) {
-     	print_othello[i][j] = b;    	
+     	gameboard[i][j];     	
 	 }
 
-print_othello(함수);  // for문을 활용- N줄의 보드판을 출력하는 함수   
-   
-printf("-------------------------\n");
+    // 게임 초기화_ 정중앙 4칸에 흰색 검정색 알을 2개씩 대각선 모양으로 배치
+void init_othello(char gameboard[][N])
+{
+
+   gameboard[N/2][N/2] = b;
+   gameboard[N/2][N/2 + 1] = w;
+   gameboard[N/2 + 1][N/2] = w;
+   gameboard[N/2 + 1][N/2 + 1] = b;
+
+   // 초기값을 인쇄  
+ printf("-------------------------\n");
 	for(i=0; i<N; i++)
 	{
 	 for(j=0; j<N; j++)
 	 
-    	printf("| %c ", 함수 [i][j]);
+    	printf("| %c ", gameboard[i][j]);
     	
 	 	
 	printf("|\n");	
@@ -32,5 +40,8 @@ printf("-------------------------\n");
     
     }
 
-	return 0;
+	return 0;   
+    
 }
+}
+
--- othello.c
+++ othello.c
@@ -8,8 +8,9 @@ int gameboard[N][N];
 void main(...){
   // 필요한 변수들 정의 
   int i, j;
-  char @; // 검정색 알
-  char O; // 하얀색 알 
+  char b, w;
+  b = '@'; // 검정색 알
+  w = 'O'; // 하얀색 알 
   
   for(i=0; i<N; i++)
      for(j=0; j<N; j++) {
@@ -17,8 +18,27 @@ void main(...){
 	 }
      
     // 게임 초기화_ 정중앙 4칸에 흰색 검정색 알을 2개씩 대각선 모양으로 배치
-  int init_othello[i][j]{
-  } gameboard[N/2]
+  void init_othello(char gameboard[][N])
+{
+
+   gameboard[N/2][N/2] = b;
+   gameboard[N/2][N/2 + 1] = w;
+   gameboard[N/2 + 1][N/2] = w;
+   gameboard[N/2 + 1][N/2 + 1] = b;
+
+   // 초기값을 인쇄  
+    
+     for(i=0; i<N; i++)
+     {
+	   for(j=0; j<N; j++)
+	    {
+     	printf("%c", gameboard[i][j]);
+     	printf("\n");
+        }
+
+	 }
+} 
+
    
    
    
